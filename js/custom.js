6bb97f98969246eb2cae1558a07041efb9731569
ssd98 <sanjaya.d@tstpl.lk>
2022-08-30 12:03:22 +0530

    home page complete

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
+
+        $('.mobile-menu-toggle').on('click', function () {
+            $('.student-sidebar').addClass('show-sidebar');
+            $('body').toggleClass('menu-has-opened');
+        });
+
+        $('.schedule-tabs .schedule-tab').on('click', function () {
+            $('.time-table').hide();
+            var dataId = $(this).data('id');
+            $('#' + dataId).slideDown();
+            $('.schedule-tabs .schedule-tab').removeClass('active');
+            $(this).addClass('active');
+        });
+
+		//home intro slider
+		$('.home-intro-slider').bxSlider({
+			auto: true,
+			autoControls: false,
+			stopAutoOnClick: true,
+			pager: false,
+			nextText: '',
+			prevText: ''
+		});
+
+        $('.customer-list-carousel').owlCarousel({
+			loop: true,
+			margin: 0,
+			nav: false,
+			autoplay: true,
+			autoplayHoverPause: true,
+			navText: ['Prev','Next'],
+			controls: false,
+			// center:true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				1000: {
+					items: 6
+				},
+				2000: {
+					items: 6
+				}
+			}
+        });
+    });
+})(jQuery);
